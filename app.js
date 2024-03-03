bc949a477e84ab9be70584b473a378c20f0ef6e9
ameermaviacooder <160848651+ameermaviacooder@users.noreply.github.com>
2024-03-03 04:25:49 -0800

    ( 45 questions) Famous Quote 2
    ( 45 questions) my 4th assigements work ,e   Famous Quote 2

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+let quote = "A person who never made a mistake never tried anything new";
+// famous personNAME
+let famous_person = "Albert Einstein";
+let message = (`${famous_person}once said,${quote}`);
+console.log(message);
---
+++ app.ts
@@ -0,0 +1,9 @@
+let quote:string= "A person who never made a mistake never tried anything new"
+
+// famous personNAME
+
+let famous_person:string ="Albert Einstein";
+
+  let message=(`${famous_person}once said,${quote}`);
+
+  console.log(message)
